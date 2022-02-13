f2a9d7b565ec133d6a818e853bdd1247be9d066e
Semyon <semyonmerchenko@mail.ru>
2022-02-13 19:46:29 +0300

    Added commenting for count of film watched

--- JS/app.js
+++ JS/app.js
@@ -25,6 +25,19 @@ for (let i = 0; i < 2; i++) {
     }
 }
 
+switch (true) {
+    case personsalMovieDB.count < 10:
+        alert("Просмотрено довольно мало фильмов");
+        break;
+    case personsalMovieDB.count >= 10 && personsalMovieDB.count < 30:
+        alert("Вы классический зритель");
+        break;
+    case personsalMovieDB.count >= 30:
+        alert("Вы киноман!");
+        break;
+    default:
+        alert("Произошла ошибка");
+}
 
 
 console.log(personsalMovieDB);
