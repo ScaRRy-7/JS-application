d8f5b56feec5a25f183d7191459e972e690d97bd
Semyon <semyonmerchenko@mail.ru>
2022-02-14 19:17:02 +0300

    Added function code style and new ability to enter favorite genres

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,17 @@
 "use strict";
 
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+    }
+}
+
+start();
 
 let personsalMovieDB = {
     count: numberOfFilms,
@@ -10,34 +21,57 @@ let personsalMovieDB = {
     private: false
 };
 
-
-
-for (let i = 0; i < 2; i++) {
-    const lastFilm = prompt("Один из последних просмотренных фильмов?", ""),
-          rateOfFilm = prompt("На сколько оцените его?", "");
-    
-    if (lastFilm != null && rateOfFilm != null && lastFilm != "" && rateOfFilm != "" && lastFilm.length < 50) {
-        personsalMovieDB.movies[lastFilm] = rateOfFilm;
-        console.log("done");
-    } else {
-        console.log("error");
-        i--;
+function rememerMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const lastFilm = prompt("Один из последних просмотренных фильмов?", ""),
+              rateOfFilm = prompt("На сколько оцените его?", "");
+        
+        if (lastFilm != null && rateOfFilm != null && lastFilm != "" && rateOfFilm != "" && lastFilm.length < 50) {
+            personsalMovieDB.movies[lastFilm] = rateOfFilm;
+            console.log("done");
+        } else {
+            console.log("error");
+            i--;
+        }
     }
 }
 
-switch (true) {
-    case personsalMovieDB.count < 10:
-        alert("Просмотрено довольно мало фильмов");
-        break;
-    case personsalMovieDB.count >= 10 && personsalMovieDB.count < 30:
-        alert("Вы классический зритель");
-        break;
-    case personsalMovieDB.count >= 30:
-        alert("Вы киноман!");
-        break;
-    default:
-        alert("Произошла ошибка");
+rememerMyFilms();
+
+function detectPersonalLevel() {
+    switch (true) {
+        case personsalMovieDB.count < 10:
+            alert("Просмотрено довольно мало фильмов");
+            break;
+        case personsalMovieDB.count >= 10 && personsalMovieDB.count < 30:
+            alert("Вы классический зритель");
+            break;
+        case personsalMovieDB.count >= 30:
+            alert("Вы киноман!");
+            break;
+        default:
+            alert("Произошла ошибка");
+    }
 }
 
+detectPersonalLevel();
 
-console.log(personsalMovieDB);
+function showMyDB(obj) {
+    if (obj.private == false) {
+        console.log(obj);
+    }
+}
+
+showMyDB(personsalMovieDB);
+
+function writeYourGenres() {
+    let favouriteGenres = [];
+
+    for (let i = 0; i < 3; i++) {
+        let item = prompt(`Ваш любимый жанр под номером ${i+1}`);
+        favouriteGenres.push(item);
+    }
+    console.log(favouriteGenres);
+}
+
+writeYourGenres();
